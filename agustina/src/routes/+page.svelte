7003dc09bb99f6d3f4c05a9857a4759f54f56b42
kazturi <125850502+kazturi302@users.noreply.github.com>
2023-03-01 14:38:45 +0700

    Update +page.svelte

--- agustina/src/routes/+page.svelte
+++ agustina/src/routes/+page.svelte
@@ -117,7 +117,7 @@
 					<div class="animate__animated animate__fadeIn animate__delay-2s">
 						    <div class="mb-10 text-center">
 							<span class="animate__animated animate__fadeIn animate__delay-2s inline-block py-1.5 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-red text-green rounded">Simple Dapp</span>
-							<div class="mt-4 animate__animated animate__fadeIn animate__delay-2s bg-brown-300 rounded-lg py-5 px-6 mb-4 text-base text-white-800 mb-3" role="alert">Click button below to begin CRUD Transaction.</div>
+							<div class="mt-4 animate__animated animate__fadeIn animate__delay-2s bg-brown-300 rounded-lg py-5 px-6 mb-4 text-base text-gold-800 mb-3" role="alert">Click Button Below Start The Transaction.</div>
 						    </div>
 						    <div class="grid animate__animated animate__fadeIn animate__delay-2s grid-cols-4 gap-4">
 							<label for="modal-create" class="btn btn-accent btn-outline">Create</label>
